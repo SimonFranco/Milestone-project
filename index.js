592a00219c6f7db4a3080d27af8886fbdb1b64c7
Simon Franco <simonfranco0@gmail.com>
2022-08-27 22:30:13 -0700

    updated

--- index.js
+++ index.js
@@ -4,59 +4,84 @@ for (var i=27;i>0;i--){
     slider.setAttribute('id','slider'+i)
     document.getElementById('game').append(slider);
 }
-var aniWidth = 500;
 var win = document.getElementById('slider26');
 win.classList.remove('animate')
 var level = document.getElementById('slider27')
 level.classList.remove('animate')
 level.textContent = 0;
 
-function winner(){
-    alert('you win!')
-}
-
-function stopSliding(slider){
-        if (slider== 26){
-            winner()
-            location.reload()
-        }
-    level.textContent = slider;
+var aniWidth = 500;
     
-    var score ='Score:'.concat(slider-1);
-    var sliderCurrent = document.getElementById('slider'.concat(slider))
-    var sliderAbove = document.getElementById('slider'.concat(slider+1))
-    if(slider ==1){
-        var sliderBelow = sliderCurrent
-    }else{
-        var sliderBelow = document.getElementById('slider'+ (slider-1 ))
-    }
-    var left = window.getComputedStyle(sliderCurrent).getPropertyValue('left');
-    sliderCurrent.classList.remove('animate');
-    // Keeps slider where clicked
-    sliderCurrent.style.left = left;
-    var width = parseInt(window.getComputedStyle(sliderCurrent).getPropertyValue('width'));
-    var leftBelow = parseInt(window.getComputedStyle(sliderBelow).getPropertyValue('left'))
-    left = parseInt(left);
-    var difference = left - leftBelow;
-    var absDifference = Math.abs(difference);
-    if( difference>width || difference <-width){
-            alert(score)
-            location.reload()
+    function stopSliding(slider){
+        level.textContent = slider;
+        if (slider== 25){
+           let h12 = document.getElementById('h12');
+        //    h12.append(score - 1)
+           let modal2 = document.getElementById('wonModal');
+           modal2.style.display = 'block';
+           level.style.visibility = 'hidden'
+        //    h2.append()
         }
-        //aligns sliders to the left
-        if (difference <0){
-            left = left + absDifference;
-            sliderCurrent.style.left = left.toString().concat("px")
+        
+        var score ='Your score is '.concat(slider-1);
+        var sliderCurrent = document.getElementById('slider'.concat(slider))
+        var sliderAbove = document.getElementById('slider'.concat(slider+1))
+        if(slider ==1){
+            var sliderBelow = sliderCurrent
+            sliderBelow.style.visibility = 'visible'
         }else{
+            var sliderBelow = document.getElementById('slider'+ (slider-1 ))
+        }
+        var left = window.getComputedStyle(sliderCurrent).getPropertyValue('left');
+        sliderCurrent.classList.remove('animate');
+        // Keeps slider where clicked
+        sliderCurrent.style.left = left;
+        var width = parseInt(window.getComputedStyle(sliderCurrent).getPropertyValue('width'));
+        var leftBelow = parseInt(window.getComputedStyle(sliderBelow).getPropertyValue('left'))
+        left = parseInt(left);
+        var difference = left - leftBelow;
+        var absDifference = Math.abs(difference);
+
+
+        //aligns sliders to the left
+        if (difference <0 ){
+            left = left + absDifference;
+            sliderCurrent.style.left = left.toString().concat("px");
+        }
+        if(difference > 0 ){
             left = left - difference;
             sliderCurrent.style.left = left.toString().concat('px');
         }
-        var offset = (width - absDifference).toString().concat('px');
-        sliderCurrent.style.width = offset;
-        sliderAbove.style.width = offset;
-        sliderAbove.style.visibility = 'visible';
-        var newWidth = aniWidth + absDifference;
-        document.documentElement.style.setProperty('--width', newWidth + 'px');
+        // if you lose
+        if( difference>width || difference <-width){
+            let h11 = document.getElementById('h11');
+            h11.append(slider -1 )
+            if(h11.textContent !==''){
+                document.getElementById('btn').disabled = true;
+            }
+            let modal1 = document.getElementById('lostModal');
+             modal1.style.display = 'block';
+             sliderAbove.classList.remove('animate');
+             sliderCurrent.style.visibility = 'hidden';
+        }
+             
+             
+ 
+            sliderAbove.style.visibility = 'visible';
+            var offset = (width - absDifference).toString().concat('px');
+            sliderCurrent.style.width = offset;
+            sliderAbove.style.width = offset;
+        
+        
+        console.log(aniWidth)
+        aniWidth = aniWidth + absDifference;
+        console.log(aniWidth, absDifference)
+        document.documentElement.style.setProperty('--width', aniWidth + 'px')
+        
         var onclick =  'stopSliding(' + (slider+1) + ')';
-        document.getElementById('btn').setAttribute('onclick', onclick);
+         if(difference<width || difference >-width){
+             document.getElementById('btn').setAttribute('onclick', onclick);
+
+         }
+
     }
